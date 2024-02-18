b68ad8b9b700758c303eddfa2a8519d7ebeca4d0
namthse01 <namthse01@gmail.com>
2024-02-18 10:46:51 +0700

    Make paymentSummary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions,getDeliveryOption} from '../../data/delivery-option.js';
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 function updateCartQuantity(){
   let cartQuantity = 0;
@@ -122,6 +123,8 @@ export function renderOrderSummary(){
       const container = document.querySelector(`.js-cart-item-container-${productId}`);
       updateCartQuantity();
       container.remove();
+
+      renderPaymentSummary();
     })
   })
 
@@ -130,6 +133,7 @@ export function renderOrderSummary(){
       const {productId, deliveryId} = element.dataset;
       updateDeliveryOption(productId,deliveryId);
       renderOrderSummary();
+      renderPaymentSummary();
     })
   })
 }
